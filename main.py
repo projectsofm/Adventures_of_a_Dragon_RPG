2a60b4356b30cf26a531878d17e3bf0f16dad1a9
projectsofm <92150799+projectsofm@users.noreply.github.com>
2022-07-17 12:01:45 -0500

    The Game

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+impoxrt os
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.maxhealth = 100
+        self.health = self.maxhealth
+        self.attack = 5
+        
+def main():
+    os.system('cls')
+    print("Adventures of a Dragon\n")
+    print("1) Start")
+    print("2) Load")
+    print("3) Exit")
+    option = input("> ")
+    if option == "1": 
+        start()
+    elif option == "2": 
+        pass
+    elif option == "3":
+        sys.exit()
+    else:
+        print("..Hey! That's not an option!\n")
+        main()
+        
+def start():
+    os.system('cls')
+    print("So the game begins...You are a dragon, and your name is..")
+    option = input("> ")
+    global Player1
+    Player1 = Player(option)
+    start1()
+
+def start1():
+    os.system('cls')
+    print("Welcome, %s." % Player1.name)
+    
+main()
